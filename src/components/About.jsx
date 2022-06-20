a0eef7ed4a7b2d2ab9effb21b3eb594234d9f98f
Mohamed <61628964+molaraiche@users.noreply.github.com>
2022-06-20 23:41:00 +0100

    set Social and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Modal from 'g@mui/material/Modal';
+import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
