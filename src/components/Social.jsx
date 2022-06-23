72dc6ab6db474b03352a2c14dd1ef06ede55275f
Mohamed <61628964+molaraiche@users.noreply.github.com>
2022-06-23 15:43:26 +0100

    set mailing

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useState, useRef } from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import MailIcon from '@mui/icons-material/Mail';
@@ -22,7 +22,13 @@ const style = {
 };
 
 const Social = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState(false);
+  const [email, setEmail] = useState(false);
+  const [msg, setMsg] = useState(false);
+  console.log(`name ${name}`);
+  console.log(`email ${email}`);
+  console.log(`msg ${msg}`);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const classes = UseStyles();
@@ -40,10 +46,23 @@ const Social = () => {
 
     form.current.reset();
   };
+  const onNamechange = () => {
+    setName(true);
+  };
+  const onEmailChange = () => {
+    setEmail(true);
+  };
+  const onMsgChange = () => {
+    setMsg(true);
+  };
   const thanksMsg = () => {
-    if (sendEmail) {
-      alert('message sent');
-      
+    if (name && email && msg) {
+      alert('msg sent');
+      setName(false);
+      setEmail(false);
+      setMsg(false);
+    } else {
+      alert('msg not sent');
     }
   };
   return (
@@ -97,8 +116,9 @@ const Social = () => {
                       required
                       type='text'
                       name='full_name'
-                      placeHolder='Full Name'
-                      id=''
+                      placeholder='Full Name'
+                      id='name'
+                      onChange={onNamechange}
                     />
                   </div>
                   <div className='form-grp'>
@@ -107,17 +127,19 @@ const Social = () => {
                       type='email'
                       name='email'
                       placeholder='Email'
-                      id=''
+                      id='email'
+                      onChange={onEmailChange}
                     />
                   </div>
                   <div className='form-grp'>
                     <textarea
                       required
                       name='message'
-                      placeHolder='Message'
-                      id=''
+                      placeholder='Message'
+                      id='msg'
                       cols='30'
                       rows='8'
+                      onChange={onMsgChange}
                     ></textarea>
                   </div>
                   <div className='form-Btn'>
